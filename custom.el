d99906e456bc8b03dc2e676daf235fb8c0f653a5
Ilya Yanok <ilya.yanok@gmail.com>
2013-11-17 17:11:30 +0100

    custom: settings for maxframe

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mf-display-padding-height 40)
+ '(mf-display-padding-width 50)
+ '(mf-offset-x 50)
+ '(mf-offset-y 20))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
